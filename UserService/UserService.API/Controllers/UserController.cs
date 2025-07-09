43933008afa9314215270c0e9723af1c428c6b3c
210280107074 <chotaliyadhruvil2309@gmail.com>
2025-07-09 11:53:04 +0530

    Response file removed

--- UserService/UserService.API/Controllers/UserController.cs
+++ UserService/UserService.API/Controllers/UserController.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+
 using System.Net;
-using System.Threading.Tasks;
+using Microservices.Shared;
 using Microsoft.AspNetCore.Mvc;
 using UserService.Application.DTOs;
 using UserService.Application.Interfaces;
-using UserService.Domain.Models;
+
 
 namespace UserService.API.Controllers
 {
