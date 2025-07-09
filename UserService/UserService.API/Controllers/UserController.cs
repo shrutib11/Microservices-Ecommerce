b40cd083c17a9e0891ade64f7a3c46c0a770e7e0
210280107074 <chotaliyadhruvil2309@gmail.com>
2025-07-09 11:45:22 +0530

    user model creation using code first approch

---
+++ UserService/UserService.API/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Application.DTOs;
+using UserService.Application.Interfaces;
+using UserService.Domain.Models;
+
+namespace UserService.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpGet(Name = "GetAllUsers")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<UserDto>))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _userService.GetAllUsers());
+        }
+        
+
+        [HttpGet("{id}", Name = "GetUserById")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserDto))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<APIResponse> GetId(int id)
+        {
+            UserDto? model = await _userService.GetUserById(id);
+            if (model == null)
+            {
+                throw new KeyNotFoundException($"User with ID {id} not found.");
+            }
+            return new APIResponse
+            {
+                StatusCode = (HttpStatusCode)StatusCodes.Status200OK,
+                Result = model,
+                IsSuccess = true
+            };
+        }
+    }
+}
--- UserService/UserService.API/Program.cs
+++ UserService/UserService.API/Program.cs
@@ -1,18 +1,47 @@
+using System.Reflection;
+using Microservices.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using UserService.Application.Interfaces;
+using UserService.Application.Mappings;
+using UserService.Domain.Interfaces;
+using UserService.Infrastructure;
+using UserService.Infrastructure.Repositories;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var conn = builder.Configuration.GetConnectionString("SupportDeskConnection");
+builder.Services.AddDbContext<UserServiceDbContext>(options => options.UseNpgsql(conn));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService.Application.Services.UserService>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly(), typeof(UserProfile).Assembly);
+
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+        c.CustomOperationIds(apiDesc =>
+    {
+        return apiDesc.TryGetMethodInfo(out MethodInfo methodInfo) ? methodInfo.Name : null;
+    });
+    });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+
     app.UseSwaggerUI();
 }
 
@@ -21,5 +50,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseMiddleware<ErrorHandlingMiddleware>();
 
 app.Run();
