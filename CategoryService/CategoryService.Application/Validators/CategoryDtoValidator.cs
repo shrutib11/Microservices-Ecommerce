c850feb8bbe6a6ec73dc76554eb11b35e16d9369
ShrutiTatva <tatva.pcq105@outlook.com>
2025-07-11 14:47:49 +0530

    Category validations

---
+++ CategoryService/CategoryService.Application/Validators/CategoryDtoValidator.cs
@@ -0,0 +1,21 @@
+using CategoryService.Application.DTOs;
+using FluentValidation;
+
+namespace CategoryService.Application.Validators;
+
+public class CategoryDtoValidator : AbstractValidator<CategoryDto>
+{
+    public CategoryDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Please provide Category Name.")
+            .MaximumLength(50);
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Please provide Category Description.")
+            .MaximumLength(200);
+
+        RuleFor(x => x.CategoryFile)
+            .NotEmpty().WithMessage("Please Provide Category Image.");
+    }
+}
